54c59fbfef9181d5597412cf2cc8da9e39e16939
Erick-Pereira <erickbaptistapereira@gmail.com>
2025-05-20 22:21:52 -0300

    Ajuste ng lint

--- src/app/features/pet-documents/pet-document/pet-document.component.ts
+++ src/app/features/pet-documents/pet-document/pet-document.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-pet-document',
   templateUrl: './pet-document.component.html',
-  styleUrls: ['./pet-document.component.scss']
+  styleUrls: ['./pet-document.component.scss'],
 })
-export class PetDocumentComponent implements OnInit {
+export class PetDocumentComponent {
+  // implements OnInit {
+  // o codigo acima deve ser implementado após PetDocumentComponent
 
-  constructor() { }
+// quando for usar esta pagina retirar os comentarios e fazer o import
+// import { OnInit } from '@angular/core';
 
-  ngOnInit(): void {
-  }
-
+  // constructor() { }
+  // ngOnInit(): void {
+  // }
 }
