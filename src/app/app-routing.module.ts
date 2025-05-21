726e1ca97585d0c15c7c6103d5b74ecd20f802c2
Matheus Avelino <avelinosantos09@gmail.com>
2025-05-20 21:52:55 -0300

    feat: iniciado tela documentos dos pets
    - Configuração inicial do componenete da página e router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { AboutComponent } from './features/about/about.component';
 import { HomeComponent } from './features/home/home.component';
 import { ServicesComponent } from './features/services/services.component';
 import { LoginComponent } from './features/login/login.component';
+import { PetDocumentComponent } from './features/pet-documents/pet-document/pet-document.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'services', component: ServicesComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'pet-documents', component: PetDocumentComponent },
 ];
 
 @NgModule({
---
+++ src/app/features/pet-documents/pet-document/pet-document.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pet-document',
+  templateUrl: './pet-document.component.html',
+  styleUrls: ['./pet-document.component.scss']
+})
+export class PetDocumentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
